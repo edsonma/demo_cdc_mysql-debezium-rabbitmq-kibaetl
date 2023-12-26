80c76f5ca9a942df721f3eca77a756e2b68bca2f
edsonma@gmail.com <edsonma@gmail.com>
2023-12-26 18:44:15 -0300

    refact: change class and separate each tranform call

--- lib/etl/jobs/kiba_transform/validate_message_transform.rb
+++ lib/etl/jobs/kiba_transform/validate_message_transform.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+require 'json'
+
 class ValidationError < StandardError; end
 
-class ValidateMessageService
-  def call!(data)
+class ValidateMessageTransform
+  def process(data)
+    puts '[transform] Transforming data...'
+    puts "\t[transform] Validating data..."
+
     raise ValidationError, "no data error." if data.nil?
 
     json_string = data.is_a?(String) ? data : data.to_json
@@ -17,16 +22,14 @@ class ValidateMessageService
     end
 
     validate_before_after_values(json)
+
+    data
   end
 
   private
 
   # Need to validate before and after values
   def validate_before_after_values(json)
-    json['after'].each do |key, value|
-      if value['answer'] == 0
-        raise ValidationError, "Invalid value for #{key} - #{value['answer']}"
-      end
-    end
+    raise(ValidationError, "Invalid value for before - #{json.dig('before', 'answer')}") if json.dig('before', 'answer') == 0 
   end
 end
