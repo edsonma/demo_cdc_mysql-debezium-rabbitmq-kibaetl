dccc935abf4277f7b5e3efabf24732d2c7c35006
edsonma@gmail.com <edsonma@gmail.com>
2023-12-26 16:52:34 -0300

    add more validation

--- lib/services/validate_message_service.rb
+++ lib/services/validate_message_service.rb
@@ -8,10 +8,25 @@ class ValidateMessageService
 
     json_string = data.is_a?(String) ? data : data.to_json
 
+    json = {}
+
     begin
-      JSON.parse(json_string)
+      json = JSON.parse(json_string)
     rescue JSON::ParserError
       raise ValidationError, "Parsing error."
+    end
+
+    validate_before_after_values(json)
+  end
+
+  private
+
+  # Need to validate before and after values
+  def validate_before_after_values(json)
+    json['after'].each do |key, value|
+      if value['answer'] == 0
+        raise ValidationError, "Invalid value for #{key} - #{value['answer']}"
+      end
     end
   end
 end
